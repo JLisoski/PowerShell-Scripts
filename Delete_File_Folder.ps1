1d3da42e62e5510d4c933930d8f6e4ed5c5bf26f
Joshua Lisoski <56094425+JLisoski@users.noreply.github.com>
2023-03-05 01:39:40 -0800

    Create Delete_File_Folder.ps1
    Script to delete files/folders within a given path that have not been accessed in 30+ days.

---
+++ Delete_File_Folder.ps1
@@ -0,0 +1,25 @@
+#Using the -Directory property will only grab Folders, while -File will only grab Files
+#$Folders = Get-ChildItem -Path <#file path#> -Directory
+$Files = Get-ChildItem -Path <#file path#> -File
+
+#Grabs the current date
+$TodaysDate = Get-Date
+
+#For each file in the files array, iterate through them
+Foreach($File in $Files){
+
+    #Grab time difference between todays date and files creation date, converted to Total Days (Decimal Format) 
+    $timeDiff = ($TodaysDate - $File.CreationTime).TotalDays
+
+    #For Debugging
+    #Write-Warning $timeDiff.TotalDays
+
+    #Check if time difference is greater then 30 days 
+    if($timeDiff -gt 30){
+        #For Debugging
+        #Write-Host $File.Name
+
+        #Removes file permanently (not sent to recycle bin)
+        Remove-Item $File
+    }
+}
